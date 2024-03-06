6717e2544f5554881c595a038c5e60b722b2c9bd
aadmin <collin-chua@hotmail.com>
2024-03-06 21:25:48 +0800

    Did some basic operations normally would do in other languages

--- Learn_Java(Self-Learn)/src/App.java
+++ Learn_Java(Self-Learn)/src/App.java
@@ -1,5 +1,33 @@
+import java.util.List;
+
+import The_Basic.Basic;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+    }
+    
+    // Dumping ground of knowledge
+    public void oldMethods(){
+        // System.out.println("Hello, World!");
+    
+        // Instantilise Object class
+        Basic basic = new Basic(420, "Johnathon", 1.96, false);
+        List<Integer> list = basic.getList();
+    
+        // Basic operation of list
+        list.add(10);
+        list.add(20);
+        list.add(30);
+    
+        // Basic control flow
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+    
+        for (int i = list.size() - 1; 0 <= i; i--){
+            System.out.println(i);
+        }
+        
     }
 }
---
+++ Learn_Java(Self-Learn)/src/The_Basic/Basic.java
@@ -0,0 +1,43 @@
+package The_Basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basic {
+    // Types
+    private int age = 69420;
+    private String name = "Collin";
+    private Double digits = 3.142;
+    private boolean binaries = true;
+
+    // List
+    List<Integer> applicationList = new ArrayList<Integer>();
+
+    public Basic(int age, String name, double digits, Boolean binaries){
+        this.age = age;
+        this.name = name;
+        this.digits = digits;
+        this.binaries = binaries;
+    }
+
+    // Functions
+    public int getAge(){
+        return this.age;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getDigits(){
+        return this.digits;
+    }
+
+    public boolean getBinaries(){
+        return this.binaries;
+    }
+
+    public List<Integer> getList(){
+        return this.applicationList;
+    }
+}
