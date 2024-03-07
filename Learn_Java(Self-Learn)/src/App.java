a5c7cae67c186fff0adc529253b8f8cf3886a54f
aadmin <collin-chua@hotmail.com>
2024-03-07 19:12:40 +0800

    Added try...catch block

--- Learn_Java(Self-Learn)/src/App.java
+++ Learn_Java(Self-Learn)/src/App.java
@@ -9,52 +9,6 @@ import oop_concepts.Vehicle;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        try {
-            // Test case 1 for try...catch
-            // Scanner user_input = new Scanner(System.in);
-            // System.out.println("What is your numerator: ");
-            // int numerator = user_input.nextInt();
-            
-            // System.out.println("What is your denominator: ");
-            // int denominator = user_input.nextInt();
-            // user_input.close();
-            
-            // String sentence = String.format("Your computed value is %d", numerator/denominator);
-            // System.out.println(sentence);
-            
-            // Test case 2 for try...catch
-            List<Integer> list_of_numbers = new ArrayList<>();
-            boolean continue_input = true;
-            String list_lines = "";
-
-            Scanner user_input = new Scanner(System.in);
-            System.out.println("What is your number: ");
-            list_of_numbers.add(user_input.nextInt());
-            
-            while (continue_input) {
-                Character u_con_input;
-                int another_number;
-                System.out.println("Do you want to keep adding?... [Y/N]:");
-                u_con_input = user_input.next().toUpperCase().charAt(0);
-                if (u_con_input == 'N'){
-                    continue_input = false;
-                    user_input.close();
-                    break;
-                }
-                System.out.println("What is your number: ");
-                another_number = user_input.nextInt();
-                list_of_numbers.add(another_number);
-            }
-            for (Integer integer : list_of_numbers) {
-                list_lines += String.format(", %d", integer);
-            }
-            String sentence = String.format("Your list of numbers is: %s", list_lines);
-            System.out.println(sentence);
-        } catch (Exception e) {
-            // Test case 1 for try...catch
-            // System.out.println("Value cannot be divided by 0!");
-        }
     }
     
     
@@ -94,9 +48,71 @@ public class App {
         // v_1.printAvailbility();
         Car c_1 = new Car(100, "Red", "Ford", true);
         c_1.printAvailbility();
-
+        
         // Abstract(Something like a template IRL, Go see Animal.java) class
         Dog clover = new Dog("Clover", "Minced Meat");
         clover.makeSound();
+        
+        // Try... Catch block
+        try {
+            /* ---Test case 1 for try...catch--- */
+            // Scanner user_input = new Scanner(System.in);
+            // System.out.println("What is your numerator: ");
+            // int numerator = user_input.nextInt();
+            
+            // System.out.println("What is your denominator: ");
+            // int denominator = user_input.nextInt();
+            // user_input.close();
+            
+            // String sentence = String.format("Your computed value is %d", numerator/denominator);
+            // System.out.println(sentence);
+            
+            /* ---Test case 2 for try...catch--- */ 
+            //Initialise variables
+            List<Integer> list_of_numbers = new ArrayList<>();
+            boolean continue_input = true;
+            String list_lines = "";
+            Scanner user_input = new Scanner(System.in);
+        
+            // Asking the user their input
+            System.out.println("What is your number: ");
+            list_of_numbers.add(user_input.nextInt());
+        
+            // While loop to ask user if they want to continue adding to the Array
+            while (continue_input) {
+                // Initialise 
+                Character u_con_input;
+                int another_number;
+                // Asking user if they want to continue
+                System.out.println("Do you want to keep adding?... [Y/N]:");
+                u_con_input = user_input.next().toUpperCase().charAt(0);
+                // Guard clause to check if they wants to continue.
+                if (u_con_input == 'N'){
+                    continue_input = false;
+                    break;
+                }
+                // Else execute this.
+                System.out.println("What is your number: ");
+                another_number = user_input.nextInt();
+                list_of_numbers.add(another_number);
+            }
+            // For each block to print out a list.
+            for (Integer integer : list_of_numbers) {
+                list_lines += String.format(", %d", integer);
+            }
+            // Allow users to check their number in the List interface
+            System.out.println("Which index in your list you want to access:");
+            int index = user_input.nextInt()  - 1;
+        
+            user_input.close();
+        
+            System.out.println(list_of_numbers.get(index));
+            String sentence = String.format("Your list of numbers is: %s", list_lines);
+            System.out.println(sentence);
+        } catch (Exception e) {
+            // Test case 1 for try...catch
+            // System.out.println("Value cannot be divided by 0!");
+            System.out.println("Hold up...Something ain't right");
+        }
     }
 }
